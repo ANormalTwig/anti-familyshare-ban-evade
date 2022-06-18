2bcc32ce1eb19f95a29b58d80100e7999826065a
ANormalTwig <1andonlytwig@gmail.com>
2022-06-18 19:48:31 -0400

    Permaban both accounts instead of kicking new one

--- lua/ulib/modules/server/familyshare.lua
+++ lua/ulib/modules/server/familyshare.lua
@@ -1,7 +1,10 @@
 hook.Add("PlayerAuthed", "KickBannedFamilyShare", function(ply, accSteamID)
   local ownerSteamID = util.SteamIDFrom64(ply:OwnerSteamID64())
+  if ownerSteamID == accSteamID then return end
+
   local msg = ULib.getBanMessage(ownerSteamID)
   if msg then
-    game.KickID(accSteamID, "[The owner of this game copy is banned!]\n" .. msg)
+    ULib.addBan(ownerSteamID, 0, "Ban Evading")
+    ULib.addBan(accSteamID,   0, "Ban Evading")
   end
 end)
